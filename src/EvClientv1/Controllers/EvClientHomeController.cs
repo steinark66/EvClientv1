02b7593a4b05d2a560286c2e1fbeaff22454f3b3
Steinar Kristoffersen <stkr@himolde.no>
2016-05-13 15:10:04 +0200

    Client from Tut

---
+++ src/EvClientv1/Controllers/EvClientHomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.WebEncoders;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EvClientv1.Controllers
+{
+    public class EvClientHomeController : Controller
+    {
+        /* GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+
+        // GET: /EvClientHome/ 
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /EvClientHome/Welcome/ 
+
+        public string Welcome(string name, int numtimes = 1, int id = 1)
+        {
+            return HtmlEncoder.Default.HtmlEncode(
+                "Hello " + name + ", ID: " + id + " and numtimes: " + numtimes);
+        }
+    }
+}
